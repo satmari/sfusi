6c4fabfb4aa552972d1c70a2ad9b0ae06d64d0a1
satmari <satmariatila@gmail.com>
2016-11-22 13:56:54 +0100

    2016.11.22

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Sfusi Box</title>
+	<title>SFUSI Box</title>
 
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 	<link href="{{ asset('/css/css.css') }}" rel="stylesheet">
@@ -27,7 +27,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="{{ url('/') }}"><b>Sfusi Box application</b></a>
+				<a class="navbar-brand" href="{{ url('/') }}"><b>SFUSI Box application</b></a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
